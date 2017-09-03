6893d49cc8861adcbff4c0689f9eca19e905f484
Jessica Barclay <jessicabarclay.net@gmail.com>
2017-09-03 18:27:40 +0100

    Add MongoDB database - connected to MongoLab cloud service - collections from form sent to MongoLab db

--- server.js
+++ server.js
@@ -1,17 +1,29 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+
+var db;
+
+MongoClient.connect('mongodb://jessicabarclay:pass@ds123614.mlab.com:23614/quotes', (err, database) => {
+	if (err) return console.log(err);
+	db = database;
+	app.listen(3000, () => {
+		console.log('listening on port 3000')
+	});
+});
 
 app.use(bodyParser.urlencoded({extended: true}))
-
-app.listen(3000, () => {
-	console.log('listening on port 3000')
-});
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/quotes', (req, res) => {
-	console.log(req.body)
+	db.collection('quotes').save(req.body ,(err, results) => {
+		if (err) return console.log(err);
+
+		console.log('saved to the database');
+		res.redirect('/');
+	});
 });
